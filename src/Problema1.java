a0ca38e59dc3334422a5175a845a38a464f6a582
Mario Cavero Pérez <mario.caveroperez@iesfm.onmicrosoft.com>
2022-03-04 08:29:40 +0100

    Signed-off-by: Mario Cavero Pérez <mario.caveroperez@iesfm.onmicrosoft.com>

---
+++ src/Problema1.java
@@ -0,0 +1,19 @@
+public class Problema1 {
+
+    public String[] cadena_del_1_al_20 (Integer numeros){
+        String[] cadena = new String[numeros];
+        for (int i = 0; i < cadena.length; i++) {
+            int x = 0;
+            i = x + 1;
+        }
+        return cadena;
+    }
+    public String cambiarNumerosPorPalabras (){
+        String[] cadena = cadena_del_1_al_20();
+        return null;
+    }
+    public boolean esNumeroPar (Integer n){
+        return n % 2 == 0;
+    }
+
+}
---
+++ test/TestProblema1.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class TestProblema1  {
+    @Test
+    public void cadena_1_al_20(Integer numeros){
+        String[] cadena = new String[numeros];
+
+    }
+}
